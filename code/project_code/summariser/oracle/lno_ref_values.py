d954acf1b668fae83b6098f37db2b77b40e25db0
James Stephenson <12j4mes@gmail.com>
2023-08-07 19:40:14 +0100

    black formatting

--- code/project_code/summariser/oracle/lno_ref_values.py
+++ code/project_code/summariser/oracle/lno_ref_values.py
@@ -9,8 +9,13 @@ class SimulatedUser:
         self.temperature = m
 
     def getPref(self, idx1, idx2):
-        # print('idx1 = %i, idx2 = %i, available reference vals =%i' % (idx1, idx2, len(self.ref_values)))
-        prob = sigmoid(self.ref_values[idx1] - self.ref_values[idx2], self.temperature)
+        # print(
+        #     "idx1 = %i, idx2 = %i, available reference vals =%i"
+        #     % (idx1, idx2, len(self.ref_values))
+        # )
+        prob = sigmoid(
+            self.ref_values[idx1] - self.ref_values[idx2], self.temperature
+        )
 
         if random.random() <= prob:
             return 0  # summary1 is preferred
